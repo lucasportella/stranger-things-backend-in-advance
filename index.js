d03f204e5220f8ed533feade696b36cfb989b7b5
lucas <lucasportellaagu@gmail.com>
2021-09-22 19:21:49 -0300

    fix .env variables in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -32,6 +33,8 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
+const { PORT } = process.env;
+
 app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+  console.log(`Escutando na porta ${PORT}`);
 });
